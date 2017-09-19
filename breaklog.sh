c68171183128e7d1e1ef622fb64360c28ba23e3d
Mesoterra <mesoterra@pickwhatever.com>
2017-09-19 18:46:16 -0400

    A simple bash break logger

--- breaklog.sh
+++ breaklog.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
+#Function to clear all variables
+CLEAR_ALL () {
+unset PRIMARY_LOG_DIR
+unset DATED_LOG_DIR
+unset DAY
+unset WHY_BREAK
+unset PRIMARY_REASON
+unset BREAK
+unset LUNCH
+unset PUNCH_BACK
+unset SECONDARY_REASON
+unset LUNCH_DURATION
+unset BREAK_1
+unset BREAK_2
+unset BREAK_3
+}
+
+#Flush all variables to stoically prevent the unexpected
+CLEAR_ALL
+
+#Set initial variables
 PRIMARY_LOG_DIR="/home/`whoami`/.breaklog"
 DATED_LOG_DIR=`date "+%Y/%b/"`
 DAY=`date "+%d"`
 WHY_BREAK="`date "+%F:%T"` :: "
-unset LUNCH
-unset BREAK
+LUNCH_DURATION="30"
+BREAK_1="5"
+BREAK_2="10"
+BREAK_3="15"
 
+#Make log dir and file
 mkdir -p "$PRIMARY_LOG_DIR/$DATED_LOG_DIR"
 touch "$PRIMARY_LOG_DIR/$DATED_LOG_DIR/$DAY.log"
 
+#Break type menu
 echo "Welcome to Breaklog, select a reason"
 echo "1) Work related?"
 echo "2) A break?"
-echo "3) Other?"
+echo "3) Lunch time?"
+echo "4) End of day wrap up?"
+echo "5) Coming back?"
+echo "6) Other?"
 read PRIMARY_REASON
 
+#Menu selection parsing
 case $PRIMARY_REASON in
  1)
   WHY_BREAK+="Work related :: "
@@ -24,45 +53,52 @@ case $PRIMARY_REASON in
   BREAK="1"
   ;;
  3)
-  WHY_BREAK+="Other specified, read note :: "
+  WHY_BREAK+="Lunch :: "
+  LUNCH="1"
+  ;;
+ 4)
+  WHY_BREAK+="End of day wrap up :: "
+  ;;
+ 5)
+  WHY_BREAK+="##Punch Back##"
+  PUNCH_BACK="1"
+  ;;
+ 6)
+  WHY_BREAK+="Other :: "
   ;;
  *)
   echo "Please start over and pick a valid reason"
+  CLEAR_ALL
   exit
 esac
 
+#Logging function for punching back in
+if [[ $PUNCH_BACK == '1' ]] ; then
+ echo "$WHY_BREAK" >> "$PRIMARY_LOG_DIR/$DATED_LOG_DIR/$DAY.log"
+ echo
+ echo "$WHY_BREAK"
+ echo
+ CLEAR_ALL
+ exit
+fi
+
+#Additional information about break
 echo "Why are you doing this?"
 read SECONDARY_REASON
 
-if [[ $SECONDARY_REASON == *"Lunch"* ]] || [[ $SECONDARY_REASON == *"lunch"* ]] ; then
- echo "Is this a lunch break?"
-  read LUNCH
-  case $LUNCH in
-   yes)
-    LUNCH="1"
-    unset BREAK
-    ;;
-   no)
-    unset LUNCH
-    ;;
-   *)
-    echo "Start over and pick a valid option"
-    exit
-    ;;
-  esac
-fi
-
 WHY_BREAK+=$SECONDARY_REASON
 
+#Log it all
 echo
 echo "$WHY_BREAK" >> "$PRIMARY_LOG_DIR/$DATED_LOG_DIR/$DAY.log"
 echo "$WHY_BREAK"
 echo
 if [[ $LUNCH == "1" ]] ; then
- echo "Your lunch will end at $(date "+%F:%T" --date='30 minutes')"
+ echo "Your lunch will end at $(date "+%F:%T" --date=$LUNCH_DURATION\ minutes)"
 fi
 if [[ $BREAK == "1" ]] ; then
- printf "Take note of the below return times.\n15 minutes: $(date "+%F:%T" --date='15 minutes')\n10 minutes: $(date "+%F:%T" --date='10 minutes')\n5 minutes: $(date "+%F:%T" --date='5 minutes')"
+ printf "Take note of the below return times.\n15 minutes: $(date "+%F:%T" --date=$BREAK_3\ minutes)\n10 minutes: $(date "+%F:%T" --date=$BREAK_2\ minutes)\n5 minutes: $(date "+%F:%T" --date=$BREAK_1\ minutes)\n"
 fi
 echo
 echo "Logged"
+CLEAR_ALL
